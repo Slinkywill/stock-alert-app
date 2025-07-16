cfc96455db1f7b1c120334eb28a114930b72a3c3
Will Downing <will@Wills-MacBook-Air.local>
2025-07-16 15:53:19 +0200

    Updated to load Firebase key from Streamlit secrets

--- register_app.py
+++ register_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import requests
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Firebase Admin Init (for Firestore)
+# Initialize Firebase from Streamlit Secrets
 if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
+    firebase_key = json.loads(st.secrets["FIREBASE_KEY_JSON"])
+    cred = credentials.Certificate(firebase_key)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
